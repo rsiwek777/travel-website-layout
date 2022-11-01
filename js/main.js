01fa7d1d8a4701c050657c0c5a5e68c1a7605240
unknown <insired@gmail.com>
2022-11-01 19:14:42 +0100

    last edits

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
 const navItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
 
 navBtn.addEventListener('click', () => {
 	nav.classList.toggle('nav--active')
@@ -18,3 +21,9 @@ navItems.forEach(e =>
 		nav.classList.remove('nav--active')
 	})
 )
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+handleCurrentYear()
